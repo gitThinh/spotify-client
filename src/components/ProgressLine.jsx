1f6472461d0b41226b9c5074ab3a0a4fa9e7a425
gitThinh <111752628+gitThinh@users.noreply.github.com>
2023-09-21 14:07:50 +0700

    sau update nhưng bị lỗi

--- src/components/ProgressLine.jsx
+++ src/components/ProgressLine.jsx
@@ -1,9 +1,43 @@
 import { useEffect, useState } from "react";
 
 
-const ProgressLine = ({ playingSong, audioRef, urlSong }) => {
+const ProgressLine = ({ playingSong, audioRef }) => {
     const [duration, setDuration] = useState()
-    
+    const [urlSong, setUrlSong] = useState()
+
+    useEffect(() => {
+        playingSong && (
+            fetch(`http://nth-audio.site/api/resources/audio/${playingSong.file_name}`, {
+                method: 'GET',
+                headers: {
+                    'x-api-key': 'c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2'
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok')
+                    }
+                    return response.blob();
+                })
+                .then(blob => {
+                    setUrlSong(URL.createObjectURL(blob))
+                })
+        )
+    }, [playingSong])
+
+
+    // add song onto audio 
+    useEffect(() => {
+        if (audioRef.current.querySelector('source')) {
+            audioRef.current.removeChild("source");
+        }
+        var source = document.createElement('source')
+        source.src = urlSong // Thay đổi đường dẫn thành tệp âm thanh thực tế
+        source.type = 'audio/mpeg'
+
+        audioRef.current.appendChild(source);
+    },[playingSong])
+
 
     useEffect(() => {
         playingSong ?
@@ -25,7 +59,7 @@ const ProgressLine = ({ playingSong, audioRef, urlSong }) => {
                     duration
                 }
             </p>
-            <audio id="audio" src={playingSong && urlSong || ''} ref={audioRef}></audio>
+            <audio id="audio" ref={audioRef}></audio>
         </div>
     );
 };
