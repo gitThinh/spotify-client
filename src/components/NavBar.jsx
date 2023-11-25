2aa0373f4ef9602d832609938acc32b6fd0f82ff
gitThinh <111752628+gitThinh@users.noreply.github.com>
2023-11-25 15:31:10 +0700

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,6 @@ import { FaCompass } from 'react-icons/fa6'
 
 
 const NavBar = () => {
-    console.log('rerender nav bar')
 
 
 
@@ -28,6 +27,13 @@ const NavBar = () => {
             </nav>
             {/* components Library */}
             <div className="navBarLibrary">
+                <div className="navBarLibrary__title">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
+                        <path d="M12 0H0V2.71429H12V0ZM12 5.42857H0V8.14286H12V5.42857ZM0 13.5714H8V10.8571H0V13.5714ZM14 0V11.1014C13.69 10.9521 13.35 10.8571 13 10.8571C11.34 10.8571 10 12.6757 10 14.9286C10 17.1814 11.34 19 13 19C14.66 19 16 17.1814 16 14.9286V2.71429H19V0H14Z" fill="white" />
+                    </svg>
+                    Danh sách phát
+                </div>
+                <button className="navBarLibrary__addPlaylist" >Thêm danh sách phát</button>
             </div>
         </div>
     )
