26ec6eeda30e2a4df96025b50dc3d1e4673064db
gitThinh <111752628+gitThinh@users.noreply.github.com>
2023-11-29 22:19:00 +0700

    update navbar css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,22 +13,23 @@ const NavBar = () => {
         <div className="navBar">
             <nav className="navBarOptions" >
                 <Link to="/">
-                    <HiHome size={24} />
+                    <HiHome className='navBarOptions__icons' />
                     Home
                 </Link>
                 <Link to="/search">
-                    <HiMagnifyingGlass size={24} />
+                    <HiMagnifyingGlass className='navBarOptions__icons' />
                     Search
                 </Link>
                 <Link to="/search">
-                    <FaCompass size={22} />
+                    <FaCompass className='navBarOptions__icons' />
                     Khám Phá
                 </Link>
             </nav>
+            <span className='navBarLine'></span>
             {/* components Library */}
             <div className="navBarLibrary">
                 <div className="navBarLibrary__title">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19" fill="none">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19 19" fill="none" className='navBarOptions__icons'>
                         <path d="M12 0H0V2.71429H12V0ZM12 5.42857H0V8.14286H12V5.42857ZM0 13.5714H8V10.8571H0V13.5714ZM14 0V11.1014C13.69 10.9521 13.35 10.8571 13 10.8571C11.34 10.8571 10 12.6757 10 14.9286C10 17.1814 11.34 19 13 19C14.66 19 16 17.1814 16 14.9286V2.71429H19V0H14Z" fill="white" />
                     </svg>
                     Danh sách phát
