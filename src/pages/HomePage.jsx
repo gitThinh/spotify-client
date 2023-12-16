bf34c81c82d83a37357026c35298dbc0906ee0d7
gitThinh <111752628+gitThinh@users.noreply.github.com>
2023-12-16 20:09:58 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -105,7 +105,7 @@ const HomePage = () => {
                 },
             })
                 .then(response => response.json())
-                .then(data => data.statusCode === 200 && setRcmList(data.metadata))
+                .then(data.statusCode === 200 && setRcmList(data.metadata))
         setIsRcm(false)
     }, [playingSong])
 
