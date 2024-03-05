870b9860458cf5f500c45740e6969fc3e34fdadf
gitThinh <111752628+gitThinh@users.noreply.github.com>
2024-03-05 22:15:32 +0700

    Revert "fail"
    This reverts commit 917f2dd942fff682c6a2f4ed55cd0ca0e80888da.

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [
-    html({
-      inject: {
-        injectTo: 'head',
-        html: {
-          transform: (html) => html.replace(/\/assets\//g, './assets/'),
-        },
-      },
-    }),
-    react()
-  ],
+  plugins: [react()]
 })
 
 
